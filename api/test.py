705f7ec703a131b09d26424bdb87811492efd504
Alexey Zontov <112818147+UmbrellaPix@users.noreply.github.com>
2023-08-15 12:52:19 +0300

    fix a headers error
    fixed the error of rejecting requests due to missing headers

--- api/test.py
+++ api/test.py
@@ -4,4 +4,7 @@ from flask import jsonify
 def test(server):
     @server.route('/test', methods=['GET'])
     def test():
-        return make_response(jsonify({'OK': 'Request accept!'}), 200)
+            response = make_response(jsonify({'OK': 'Request accept!'}), 200)
+            response.headers.add('Access-Control-Allow-Origin', 'http://localhost:8080')
+            response.headers.add('Access-Control-Allow-Credentials', 'true')
+            return response
